ed1ac03a8d94219b402f6c737669620049e80155
siddharthmsingh2001 <siddharthmsingh2001@gmail.com>
2026-01-16 16:50:41 +0530

    Added backend service with Dockerfile and Health Endpoint

---
+++ backend/src/main/java/com/financelog/backend/FinanceLogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.financelog.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceLogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceLogBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/financelog/backend/FinanceLogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financelog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceLogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
