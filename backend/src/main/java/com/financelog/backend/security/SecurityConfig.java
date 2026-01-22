467b85abaf708b8821c07a14f80b242ee6f2b7fd
siddharthmsingh2001 <siddharthmsingh2001@gmail.com>
2026-01-22 18:00:52 +0530

    Added Spring Security configuration

---
+++ backend/src/main/java/com/financelog/backend/security/SecurityConfig.java
@@ -0,0 +1,71 @@
+package com.financelog.backend.security;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .securityMatcher("/**")
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(new CorsConfig()))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health", "/login", "error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/oauth2/authorization/cognito")
+                        .defaultSuccessUrl("/", true) //alternative to this is successHandler
+                        .clientRegistrationRepository(new InMemoryClientRegistrationRepository())
+                )
+                .securityContext(securityContext -> securityContext
+                        .requireExplicitSave(false)
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                );
+        return http.build();
+    }
+
+}
+/*
+ * Spring Security learns about OAuth endpoints from spring.security.oauth2.client.provider.cognito.issuer-uri
+ * Spring automatically calls the endpoint {issuer-uri}/.well-known/openid-configuration and obtains
+ * - authorization endpoint
+ * - token endpoint
+ * - JWKS endpoint
+ * - logout endpoint
+ *
+ * Flow:
+ * Spa calls: https://api.finance-log.com/oauth2/authorization/{registrationId}} (auto-created when enable .oauth2Login())
+ * Spring Redirects to cognito authorization endpoint:
+ *      https://finance-log-dev.auth.ap-south-1.amazoncognito.com/oauth2/authorize
+        ?response_type=code
+        &client_id=XXXX
+        &redirect_uri=https://api.finance-log.com/login/oauth2/code/cognito
+        &scope=openid+email+profile
+        &state=abc123
+ * User logs in / signs up at https://finance-log-dev.auth.ap-south-1.amazoncognito.com/login
+ * After success cognito redirects back to backend:
+ * https://api.finance-log.com/login/oauth2/code/cognito
+  ?code=AUTH_CODE
+  &state=abc123
+ * Spring exchanges code for tokens internally at token_endpoint
+   https://finance-log-dev.auth.ap-south-1.amazoncognito.com/oauth2/token
+ * Spring then creates a SecurityContext and stores it in HTTP Session and sends back Set-Cookie: JSESSIONID=.......
+ * Spring also sends a redirect to oauth2.defaultSuccessUrl which is matches to the endpoint on our backend.
+ * When user clicks on logout the SPA calls the backend /logout which then clears the Spring Session
+ * Spring also redirects to logout_url in cognito to clear cognito's session at
+   https://finance-log-dev.auth.ap-south-1.amazoncognito.com/logout
+ */
